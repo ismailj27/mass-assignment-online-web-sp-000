777a0f7183fdfe23339456877ee8f3fc6d7d5f75
Ismail Jalloh <ijalloh21@gmail.com>
2019-08-23 18:38:42 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,3 +4,11 @@ class Person
   def initialize
   end
 end
+
+class Person
+  attr_accessor :name, :user_name, :age, :location, :bio
+ 
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
+end
